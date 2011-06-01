b64935ef3ce3f26119d6eb2b4764146f4eeb0f6b
Toru Yamaguchi <zigorou@cpan.org>
2011-06-02 02:16:30 +0900

    initial commit

---
+++ lib/MySQL/SustainableQuery.pm
@@ -0,0 +1,32 @@
+package MySQL::SustainableQuery;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+MySQL::SustainableQuery -
+
+=head1 SYNOPSIS
+
+  use MySQL::SustainableQuery;
+
+=head1 DESCRIPTION
+
+MySQL::SustainableQuery is
+
+=head1 AUTHOR
+
+Toru Yamaguchi E<lt>zigorou at ecpan dot orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
