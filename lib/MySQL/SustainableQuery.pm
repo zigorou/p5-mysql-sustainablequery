a139701ca308fce90dbff70ae964e1561f4cc61b
Toru Yamaguchi <zigorou@cpan.org>
2011-06-02 07:54:23 +0900

    adhoc

--- lib/MySQL/SustainableQuery.pm
+++ lib/MySQL/SustainableQuery.pm
@@ -1,7 +1,77 @@
 package MySQL::SustainableQuery;
+
 use strict;
 use warnings;
+use Class::Accessor::Lite (
+    new => 0,
+    rw  => [qw/wait_interval check_strategy_interval strategy logger exec_query terminate_condition/],
+);
+use POSIX ();
+use Time::HiRes ();
+
 our $VERSION = '0.01';
+
+sub new {
+    my $class = shift;
+    my $args = ref $_[0] ? $_[0] : +{ @_ };
+    %$args = (
+        wait_interval => 0.1,
+        check_strategy_interval => 10,
+        strategy => +{
+            class => 'ByLoad',
+            args  => +{},
+        },
+        logger => sub {
+            my ( $level, @messages ) = @_;
+            for (@messages) {
+                printf("[%s %s] %s\n", $level, POSIX::strftime("%Y-%m-%d %H:%M:%S", localtime), $_);
+            }
+        },
+        exec_query => sub { 1; },
+        terminate_condition => sub { 1; },
+        %$args,
+    );
+    bless $args => $class;
+}
+
+sub run {
+    my $self = shift;
+    $self->setup_strategy();
+    my $i = 1;
+    my $time_sum = 0;
+    my $time_total = 0;
+    my $wait_interval = $self->wait_interval;
+    for (;;) {
+        my $t0 = [ Time::HiRes::gettimeofday ];
+        my $rv = $self->exec_query->( $self, $i );
+        my $elapsed = tv_interval ( $t0, [gettimeofday]);
+        $time_sum += $elapsed;
+        $time_total += $elapsed;
+
+        if ( $self->terminate_condition->( $self, $rv, $i, $time_sum ) ) {
+            # $self->log( 'INFO', sprintf('finished, execute total time: %.3f sec (counter: %d)', $time_total, $i) );
+            last;
+        }
+
+        if ( $i % $self->check_strategy_interval == 0 ) {
+            my $total_wait = $self->strategy->check( $self, $time_sum, $i );
+            $wait_interval = $self->wait_interval + $total_wait / $self->check_strategy_interval;
+            $time_sum = 0;
+        }
+
+        if ( $wait_interval > 0 ) {
+            # $self->log( 'INFO', sprintf('execute elapsed: %.3f sec, wait interval %.3f sec (counter: %d)', $elapsed, $wait_interval, $i) );
+            Time::HiRes::sleep( $wait_interval );
+        }
+
+        $i++;
+    }
+}
+
+sub log {
+    my ( $self, $level, @messages ) = @_;
+    $self->logger->( $level, @messages );
+}
 
 1;
 __END__
